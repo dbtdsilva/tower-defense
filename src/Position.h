a1ca21c30e1d1809a3a98006e9e2a738f65e0077
Diogo Silva <dbtds@ua.pt>
2016-11-30 17:10:06 +0000

    Added position template

---
+++ src/Position.h
@@ -0,0 +1,43 @@
+//
+// Created by myrddin on 30/11/16.
+//
+
+#ifndef TOWERDEFENSE_POSITION_H
+#define TOWERDEFENSE_POSITION_H
+
+#include <iostream>
+
+template <class T>
+class Position {
+public:
+    Position(T x, T y) : x_(x), y_(y) {}
+    Position(const Position& other) { *this = other; }
+    ~Position() {}
+
+    void set_x(const T& x) { this->x_ = x; }
+    void set_y(const T& y) { this->y_ = y; }
+    T& get_x() { return x_; }
+    T& get_y() { return y_; }
+
+    Position& operator=(const Position& other) {
+        if (this != &other) {
+            this->x_ = other.x_;
+            this->y_ = other.y_;
+        }
+        return *this;
+    }
+    bool operator==(const Position& other) {
+        return this->x_ == other.x_ && this->y_ == other.y_;
+    }
+    bool operator!=(const Position& other) {
+        return !(*this == other);
+    }
+    friend std::ostream& operator<< (std::ostream& os, const Position& other) {
+        os << other.x_ << ", " << other.y_;
+        return os;
+    }
+private:
+    T x_, y_;
+};
+
+#endif //TOWERDEFENSE_POSITION_H
