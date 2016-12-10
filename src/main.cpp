67eeb03bd409ef77f3ad353d44d2d1d89c6f149a
Diogo Silva <dbtds@ua.pt>
2016-12-10 18:41:58 +0000

    Added example with Cereal serializer

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <stdlib.h>
 #include <iostream>
+#include <sstream>
+#include <cereal/archives/binary.hpp>
+#include <cereal/types/vector.hpp>
+#include <cereal/types/string.hpp>
+
 #include "logic/WorldState.h"
+#include "serializer/WorldDataSerializer.h"
+
 
 using namespace std;
 
@@ -13,5 +20,19 @@ int main(int argc, char** argv) {
         cout << "Action: " << (int)change.action_ << endl;
     }
     cout << state << endl;
+
+
+    std::string sta = "abc";
+    int x = 5;
+    ostringstream stream;
+    cereal::BinaryOutputArchive archive(stream);
+    archive(sta, x);
+
+    string b;
+    int y;
+    std::stringstream stream2(stream.str());
+    cereal::BinaryInputArchive archive2(stream2);
+    archive2(b, y);
+    cout << "String: " << b << ", y: " << y;
     return EXIT_SUCCESS;
 }
