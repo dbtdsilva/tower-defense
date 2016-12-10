7d96c3a10d03e2854c8d7baed30919762b462a94
Diogo Silva <dbtds@ua.pt>
2016-12-10 19:25:27 +0000

    Updated serializer

--- src/serializer/WorldDataSerializer.h
+++ src/serializer/WorldDataSerializer.h
@@ -1,32 +1,74 @@
 #ifndef TOWERDEFENSE_WORLDDATASERIALIZER_H
 #define TOWERDEFENSE_WORLDDATASERIALIZER_H
 
+#include <cereal/archives/binary.hpp>
 #include <vector>
 #include "../helpers/Position.h"
 #include "../helpers/Definitions.h"
 
-typedef struct {
-    Position<double> position;
-    int damage;
-} BulletData;
+class BulletData {
+public:
+    BulletData() : position_(0,0), damage_(0) {}
+    BulletData(const Position<double>& pos, const int& damage) : position_(pos), damage_(damage) {}
 
-typedef struct {
-    Position<double> position;
-    MonsterType type;
-    int health;
-    double angle;
-} MonsterData;
+    Position<double> position_;
+    int damage_;
+private:
+    friend class cereal::access;
+    template<typename Archive>
+    void serialize(Archive &archive) {
+        archive(position_, damage_);
+    }
+};
 
-typedef struct {
-    Position<double> position;
-    TowerType type;
-    double angle;
-} TowerData;
+class MonsterData {
+public:
+    MonsterData() : position_(0,0), type_(MonsterType::BASIC), health_(0), angle_(0) {}
+    MonsterData(const Position<double>& position, const MonsterType& type, const int& health, const double& angle) :
+        position_(position), type_(type), health_(health), angle_(angle) {}
 
-typedef struct {
-    std::vector<TowerData> towers;
-    std::vector<MonsterData> monsters;
-    std::vector<BulletData> bullets;
-} WorldData;
+    Position<double> position_;
+    MonsterType type_;
+    int health_;
+    double angle_;
+private:
+    friend class cereal::access;
+    template<typename Archive>
+    void serialize(Archive &archive) {
+        archive(position_, type_, health_, angle_);
+    }
+};
+
+class TowerData {
+public:
+    TowerData() : position_(0,0), type_(TowerType::SIMPLE), angle_(0) {}
+    TowerData(const Position<double>& position, const TowerType& type, const double& angle) :
+            position_(position), type_(type), angle_(angle) {}
+
+    Position<double> position_;
+    TowerType type_;
+    double angle_;
+private:
+    friend class cereal::access;
+    template<typename Archive>
+    void serialize(Archive &archive) {
+        archive(position_, type_, angle_);
+    }
+};
+
+class WorldData {
+public:
+    WorldData() {}
+
+    std::vector<TowerData> towers_;
+    std::vector<MonsterData> monsters_;
+    std::vector<BulletData> bullets_;
+private:
+    friend class cereal::access;
+    template<typename Archive>
+    void serialize(Archive &archive) {
+        archive(bullets_, towers_, monsters_);
+    }
+};
 
 #endif //TOWERDEFENSE_WORLDDATASERIALIZER_H
