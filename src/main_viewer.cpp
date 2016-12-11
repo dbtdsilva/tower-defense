e75b53e64319912340fdc3662be6355edfbde52a
Diogo Silva <dbtds@ua.pt>
2016-12-11 00:05:20 +0000

    Added sample how to read world serialized

--- src/main_viewer.cpp
+++ src/main_viewer.cpp
@@ -1,3 +1,17 @@
+#include <fstream>
+#include <cereal/types/vector.hpp>
+#include <cereal/types/string.hpp>
+
+#include "serializer/WorldDataSerializer.h"
+
+using namespace std;
+
 int main() {
+    ifstream file_serialized("world_serialized.bin", ios::binary);
+    WorldData world;
+    cereal::BinaryInputArchive archive(file_serialized);
+    archive(world);
+
+    cout << world.monsters_.size() << endl;
     return 0;
 }
