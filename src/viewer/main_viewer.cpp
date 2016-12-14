f54b74c25b4379a2ffa6958a44dab2704682d124
Eduardo Sousa <eduardosousa@ua.pt>
2016-12-14 13:33:48 +0000

    Created a thread for incoming updates

--- src/viewer/main_viewer.cpp
+++ src/viewer/main_viewer.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <fstream>
+#include <thread>
 #include <cereal/types/vector.hpp>
 #include <cereal/types/string.hpp>
+
+#include <stdio.h>
 
 #include <helpers/WorldDataSerializer.h>
 #include "MapDrawer.h"
@@ -11,7 +14,7 @@ using namespace std;
 const int SCREEN_WIDTH = 700;
 const int SCREEN_HEIGTH = 600;
 
-int main() {
+void recv_message(MapDrawer *drawer) {
     ifstream file_serialized;
     file_serialized.open("/dev/rtp0", ios::binary);
 
@@ -20,25 +23,30 @@ int main() {
     unsigned long idx, idx_message;
     string value, recv, sub, message;
 
-    MapDrawer *drawer = new MapDrawer(SCREEN_WIDTH, SCREEN_HEIGTH);
-    if(!drawer->initSuccessful()) return -1;
-
     while (!drawer->isQuit()) {
         bytes_read = file_serialized.readsome(buf, 16);
 
-        if (bytes_read == 0) continue;
+        if (bytes_read == 0) 
+            continue;
+
         value = "";
+        
         for (int i = 0; i < bytes_read; i++) {
             value += buf[i];
         }
+        
         recv += value;
-
         idx = recv.rfind("MESSAGE");
-        if (idx <= 0) continue;
+        
+        if (idx <= 0) 
+            continue;
+        
         sub = recv.substr(0, idx);
-
         idx_message = sub.rfind("MESSAGE") + 7;
-        if (idx_message == -1) continue;
+        
+        if (idx_message == -1) 
+            continue;
+        
         message = sub.substr(idx_message);
         recv = recv.substr(idx);
 
@@ -50,9 +58,28 @@ int main() {
 
         drawer->updateWorldData(world);
         drawer->drawMap();
-
-        cout << world->monsters_.size() << endl; 
     }
+}
+
+int main() {
+    cout << "Sending tower" << endl;
+
+    MapDrawer *drawer = new MapDrawer(SCREEN_WIDTH, SCREEN_HEIGTH);
+
+    if(!drawer->initSuccessful()) 
+        return -1;
+
+    thread recv_handler(recv_message, drawer);
+
+    while (!drawer->isQuit()) {
+        if(drawer->handleEvents()) {
+            cout << "Putting tower..." << endl;
+
+            if(drawer->getNewTower() != nullptr) {}
+        }
+    }
+
+    recv_handler.join();
 
     delete drawer;
 
